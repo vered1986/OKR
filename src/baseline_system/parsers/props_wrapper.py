268dfcd3a7b6cde04b9cd2577277f36761cf791a
gabis <gabriel.satanovsky@gmail.com>
2017-06-26 16:06:15 +0300

    Using PropS in OKR

--- src/baseline_system/parsers/props_wrapper.py
+++ src/baseline_system/parsers/props_wrapper.py
@@ -5,5 +5,39 @@ Author: Gabi Stanovsky
 """
 
 import logging
+from pprint import pformat
 
-from props.applications.run import parseSentences
+from props.applications.run import parseSentences, load_berkeley
+
+class PropSWrapper:
+    """
+    Class to give access to PropS variables.
+    and the perform some preprocessing, where needed.
+    Works in a parse-then-ask paradigm, where sentences are first parsed,
+    and then certain inquires on them are supported.
+    """
+    def __init__(self):
+        """
+        Inits the underlying Berkeley parser.
+        """
+        load_berkeley(tokenize = False)
+
+    def parse(self, sent):
+        """
+        Parse a raw sentence - shouldn't return a value, but properly change the internal status.
+        :param sent - string, raw tokenized sentence (split by single spaces)
+        """
+        # Get PropS graph for this sentence
+        # (ignore the textual tree representation)
+        self.graph, _ = parseSentences(sent)[0]
+
+
+
+if __name__ == "__main__":
+    """
+    Simple unit tests
+    """
+    logging.basicConfig(level = logging.DEBUG)
+    pw = PropSWrapper()
+    pw.parse("hello world")
+    logging.info(pformat(pw.g))
